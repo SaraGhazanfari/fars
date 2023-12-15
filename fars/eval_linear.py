de16bded141bd487339610ca4db27f94b6c40064
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-12-15 16:21:25 -0500

    debug

--- fars/eval_linear.py
+++ fars/eval_linear.py
@@ -64,9 +64,7 @@ class LinearEvaluation:
         modified_state_dict = dict()
         if len(classifier_checkpoint) != 0:
             state_dict = torch.load(classifier_checkpoint[-1])['model_state_dict']
-            # for key, value in state_dict.items():
-            #     modified_state_dict[key.replace('module.', '')] = value
-            msg = self.linear_classifier.load_state_dict(modified_state_dict, strict=False)
+            msg = self.linear_classifier.load_state_dict(state_dict, strict=False)
             self.has_training = False
             print(f'Linear classifier is loaded! {msg}')
 
